d46013413a830aef902068a65b54f275b243f742
Phillip Creglow <phillipcreglow@msn.com>
2019-07-25 15:37:26 -0400

    JS Changes

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ $.ajax({
     data: formData,
     success: function(data) {
         console.log(data);
-        console.log(data[0]);
-        console.log(data[0]['cell.tsmart_wireless_phone'])
+        console.log(data.potential_voter_matches[0]);
+        console.log(data.potential_voter_matches[0]['cell.tsmart_wireless_phone'];
         // console.log(data_json.potential_voter_matches);
         // var potential_matches = data_json.potential_voter_matches;
         // console.log(potential_matches[0]['cell.tsmart_wireless_phone']);
