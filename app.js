7c7be0bdcf4cca560593c13e4cc7ab7be1836e66
Phillip Creglow <phillipcreglow@msn.com>
2019-07-25 13:25:02 -0400

    Core files update

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("Hello World!");
+jQuery(function($) {
+$(document).ready(function() {
+// write your code here
+$('#real_search').submit(function(e) {
+        e.preventDefault();
+$.ajax({
+    type: "GET",
+    url: 'https://github.com/pcreglow/real_data_lookup/blob/master/ts_data.php',
+    data: {
+        first_name: 'Phillip',
+        last_name: 'Creglow',
+        state: 'FL'
+    },
+    success: function(data) {
+        console.log(data);
+    }
+});
+// var voters = <?php echo $r; ?>;
+// console.log(voters.potential_voter_matches);
+// var potential_matches = voters.potential_voter_matches;
+// console.log(potential_matches[0]['cell.tsmart_wireless_phone']);
+// console.log(potential_matches[0]['vb.tsmart_first_name']);
+// console.log(potential_matches[0]['vb.tsmart_last_name']);
+//
+// for (i = 0; i <= potential_matches.length; i++) {
+//     var first_name = potential_matches[i]['vb.tsmart_first_name'];
+//     var last_name = potential_matches[i]['vb.tsmart_last_name'];
+//     var row = '<tr><td>' + first_name + '</td><td>' + last_name + '</td></tr>';
+//     jQuery('#table-container').append(row);
+// }
+
+//jQuery('#voter-results').append('<table>' + variable + '</table>')
+});
+});
+});
---
+++ ts_data.php
@@ -0,0 +1,48 @@
+<?php
+// Simple Authentication
+// x-api-key required in header
+
+$method = 'GET';
+$host = 'api.targetsmart.com';
+
+$apikey = 'XkuPmRTkvl1eUWr67zH3o9gd4YNbThHoLsP1Folc';
+$apipath = 'voter-suggest';
+
+// request parameters should be url encoded
+//$request_parameters = 'state=OR&first_name=Shawn&last_name=Cleave';
+
+$form_array = array();
+$form_array['first_name'] = $_GET["first_name"];   //use $_GET instead of $atts when in production
+$form_array['last_name'] = $_GET["last_name"];   //use $_GET instead of $atts when in production
+$form_array['state'] = $_GET["state"];   //use $_GET instead of $atts when in production
+//$form_array['example_server_side_variable'] = "some_static_variable";
+$request_parameters = http_build_query($form_array);
+
+//comment these two lines out after testing in snippets console
+//echo $request_parameters;
+//die();
+
+
+$endpoint = 'https://' . $host . '/voter/' . $apipath;
+
+$request_url = $endpoint . '?' . $request_parameters;
+
+$ch = curl_init();
+$timeout = 5;
+curl_setopt($ch, CURLOPT_URL, $request_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'x-api-key: ' . $apikey));
+
+// ************* SEND THE REQUEST *************
+echo '\nBEGIN REQUEST....................................';
+echo 'Request URL = ' . $request_url;
+$r = curl_exec($ch);
+$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+echo '\nRESPONSE....................................';
+curl_close($ch);
+echo $r;
+?>
