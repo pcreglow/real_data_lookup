2f25d3f83498c07bf7e79d0e3d553a6c307265cf
Phillip Creglow <phillipcreglow@msn.com>
2019-07-25 15:22:15 -0400

    JS update

--- app.js
+++ app.js
@@ -8,24 +8,18 @@ console.log(formData);
 $.ajax({
     type: "GET",
     url: "ts_data.php",
+    dataType: json,
     data: formData,
     success: function(data) {
         console.log(data);
-        var voters = data;
-        console.log(voters.potential_voter_matches);
-        var potential_matches = voters.potential_voter_matches;
+        console.log(data.potential_voter_matches);
+        var potential_matches = data.potential_voter_matches;
         console.log(potential_matches[0]['cell.tsmart_wireless_phone']);
         console.log(potential_matches[0]['vb.tsmart_first_name']);
         console.log(potential_matches[0]['vb.tsmart_last_name']);
     }
 });
-// var voters = <?php echo $r; ?>;
-// console.log(voters.potential_voter_matches);
-// var potential_matches = voters.potential_voter_matches;
-// console.log(potential_matches[0]['cell.tsmart_wireless_phone']);
-// console.log(potential_matches[0]['vb.tsmart_first_name']);
-// console.log(potential_matches[0]['vb.tsmart_last_name']);
-//
+
 // for (i = 0; i <= potential_matches.length; i++) {
 //     var first_name = potential_matches[i]['vb.tsmart_first_name'];
 //     var last_name = potential_matches[i]['vb.tsmart_last_name'];
