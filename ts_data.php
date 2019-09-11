816fd733304042eb6ce8e3baca14246ba1dde17e
Phillip Creglow <phillipcreglow@msn.com>
2019-09-11 11:40:26 -0400

    Refactor app.js

--- ts_data.php
+++ ts_data.php
@@ -44,5 +44,5 @@ $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
 //echo '\nRESPONSE....................................';
 curl_close($ch);
-echo json_encode(array($r));
+echo json_decode(array($r));
 ?>
