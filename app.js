8fdf13ffcbd535c35576ab6d75023ee6d28daeb8
Phillip Creglow <phillipcreglow@msn.com>
2019-09-11 13:52:04 -0400

    Refactor app.js

--- app.js
+++ app.js
@@ -25,7 +25,13 @@ function voterData(data_lookup){
   for (i = 0; i <= voter_response.length; i++) {
     console.log(voter_response[i]['ts.tsmart_presidential_primary_turnout_score']);
     console.log(voter_response[i]['vb.tsmart_last_name']);
+    var first_name = voter_response[i]['vb.tsmart_first_name'];
+    var last_name = voter_response[i]['vb.tsmart_last_name'];
+    var row = '<tr><td>' + first_name + '</td><td>' + last_name + '</td></tr>';
+    jQuery('#table-container').append(row);
   }
+
+  jQuery('#voter-results').append('<table>' + variable + '</table>');
 
   // $.each(voter_response, function(i, item) {
   //   console.log(this.['vb.tsmart_first_name']);
