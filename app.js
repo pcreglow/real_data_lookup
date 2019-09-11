029a9f3e70b21a9c49744ad79cce974fbf957cab
Phillip Creglow <phillipcreglow@msn.com>
2019-09-11 14:34:59 -0400

    Refactor app.js

--- app.js
+++ app.js
@@ -18,15 +18,17 @@ $.ajax({
 function voterData(data_lookup){
   var voters = JSON.parse(data_lookup);
   var voter_response = voters.potential_voter_matches;
-
+  var statusHTML = '<div>';
     $.each(voter_response, function(i, item){
        console.log(voter_response[i]['vb.tsmart_first_name']);
        console.log(voter_response[i]['vb.tsmart_last_name']);
         var first_name = voter_response[i]['vb.tsmart_first_name'];
         var last_name = voter_response[i]['vb.tsmart_last_name'];
-        var row = '<h4>' + first_name + ' ' + last_name + '</h4>';
-        $('#voter-results').append(row);
+        statusHTML += '<h4>' + first_name + '</h4>';
+        statusHTML += '<h4>' + last_name + '</h4>';
+        statusHTML += '</div>';
     });
+    $('#voter_results').html(statusHTML);
   // for (i = 0; i <= voter_response.length; i++) {
   //   console.log(voter_response[i]['vb.tsmart_first_name']);
   //   console.log(voter_response[i]['vb.tsmart_last_name']);
