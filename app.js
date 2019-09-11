dd3c99754429d44c03b43ff9d21b19145962942e
Phillip Creglow <phillipcreglow@msn.com>
2019-09-11 12:37:35 -0400

    Refactor app.js

--- app.js
+++ app.js
@@ -28,9 +28,11 @@ function voterData(data_lookup){
  //     jQuery('#table-container').append(row);
  // }
  // jQuery('#voter-results').append('<table>' + variable + '</table>')
- $.each(voter_response, function (name, value) {
+ $.each(voter_response, function () {
+   $.each(this, function (value) {
      console.log("First Name: " + ['vb.tsmart_first_name']);
      console.log("Last Name: " + ['vb.tsmart_last_name']);
+   });
   });
  }
 });
